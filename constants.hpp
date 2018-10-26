ee610d9171400656f0a292995c7c3cd489d0616b
Christina Brasco <christina.brasco@gmail.com>
2018-10-25 19:08:56 -0700

    comnenting out enums
    didn't realize these had been defined in Critter.hpp. We can conslidate and pick a place for them later, committing so stuff doesn't get broken

--- constants.hpp
+++ constants.hpp
@@ -2,9 +2,9 @@
 const int ASCII_INT_MIN = 48;
 const int ASCII_INT_MAX = 57;
 
-enum critterType {ANT, DOODLEBUG};
-enum direction {NONE=-1, UP, RIGHT, DOWN, LEFT};
-enum spaceValue {ANT, DOODLEBUG, EMPTY, OUTOFBOUNDS};
+//enum critterType {ANT, DOODLEBUG};
+//enum direction {NONE=-1, UP, RIGHT, DOWN, LEFT};
+//enum spaceValue {ANT, DOODLEBUG, EMPTY, OUTOFBOUNDS};
 
 char ANT_CHAR='O';
 char DOODLE_CHAR='X';
