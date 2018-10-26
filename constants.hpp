976187f2fca1ab0edf76f5c0af094a4ea7b4f1f8
Christina Brasco <christina.brasco@gmail.com>
2018-10-25 17:59:08 -0700

    constants.hpp commit

---
+++ constants.hpp
@@ -0,0 +1,11 @@
+// constants to indicate ASCII range of integers
+const int ASCII_INT_MIN = 48;
+const int ASCII_INT_MAX = 57;
+
+enum critterType {ANT, DOODLEBUG};
+enum direction {NONE=-1, UP, RIGHT, DOWN, LEFT};
+enum spaceValue {ANT, DOODLEBUG, EMPTY, OUTOFBOUNDS};
+
+char ANT_CHAR='O';
+char DOODLE_CHAR='X';
+char EMPTY_CHAR=' ';
